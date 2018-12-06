16865b5a6321e0a463eb32a5c09261ee8d65a160
Adriano Avelino <adriano-st777@hotmail.com>
2018-12-05 22:18:54 -0200

    Add practice part 01 - module 03

---
+++ module-03/practice/object.rb
@@ -0,0 +1,6 @@
+require_relative "person"
+
+pessoa1 = Person.new("Cristi Allan", 23, 1.75, "homem")
+pessoa2 = Person.new("Maria", 25, 1.63, "mulher")
+pessoa1.introduce
+pessoa2.introduce
---
+++ module-03/practice/person.rb
@@ -0,0 +1,32 @@
+class Person
+    def initialize(name, age, height, sex)
+        @name = name
+        @age = age
+        @height = height
+        @sex = sex
+    end
+  
+    def talk
+        puts "Ola"
+    end
+  
+    def walk(steps)
+        puts "#{@name} andou #{steps} passos"
+    end
+  
+    def set_name(name)
+        @name = name
+    end
+  
+    def to_sleep
+        puts "Boa noite, hora de dormir"
+    end
+  
+    def to_eat(meal)
+        puts "hora de comer #{meal}"
+    end
+  
+    def introduce
+        puts "Me chamo #{@name} tenho #{@age} anos, tenho #{@height} de altura e sou #{@sex}"
+    end
+end
