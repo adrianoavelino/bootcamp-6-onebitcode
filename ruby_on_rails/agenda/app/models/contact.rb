3da45783ad2b818f9e7fb12d6befe01742de1d46
Adriano Avelino <adriano-st777@hotmail.com>
2018-12-09 15:03:49 -0200

    add relationship in models

--- ruby_on_rails/agenda/app/models/contact.rb
+++ ruby_on_rails/agenda/app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+    has_many :phones # possui muitos telefones
+    has_one :address # possui um endereço
+   
+    has_many :contact_kinds # contact possui muitos 
+    has_many :kinds, through: :contact_kinds # contact possui muitos kinds, através de
 end
--- ruby_on_rails/agenda/app/models/kind.rb
+++ ruby_on_rails/agenda/app/models/kind.rb
@@ -1,2 +1,4 @@
 class Kind < ApplicationRecord
+    has_many :contact_kinds # possui muitos 
+    has_many :contacts, through: :contact_kinds #possui muitos contatos através de 
 end
